aec8069a21deda386cf18f375a0a0d98614ebd57
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-02-22 21:31:41 +0300

    Changed the PORT variable definition for the production and the development modes

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express');
 const path = require('path');
 const routes = require('./routes');
 const { errorHandler } = require('./middlewares');
+const { PORT: PORT_DEV } = require('./config.dev.json');
 
-const { PORT = 3000 } = process.env;
+const { PORT = PORT_DEV } = process.env;
 
 const app = express();
 
