a8cb35dde762d543db13c116a5aca5eab8279bc3
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-02-27 17:46:46 +0300

    Connected to the MongoDB mestodb database

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const path = require('path');
+
 const routes = require('./routes');
 const { errorHandler } = require('./middlewares');
-const { PORT: PORT_DEV } = require('./config.dev.json');
+const { PORT: PORT_DEV, DB: DB_DEV } = require('./config.dev.json');
 
-const { PORT = PORT_DEV } = process.env;
+const { PORT = PORT_DEV, DB = DB_DEV } = process.env;
 
 const app = express();
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
