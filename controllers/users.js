5cc9b51e1a07698b7e7589f592aa2da34884064b
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-01 17:41:03 +0300

    Created the createUser controller

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,16 @@ const getUser = (req, res, next) => {
     .catch(() => next({ status: 404, message: { message: 'Нет пользователя с таким id' } }));
 };
 
+const createUser = (req, res, next) => {
+  const { name, about, avatar } = req.body;
+
+  User.create({ name, about, avatar })
+    .then((user) => res.status(201).send(user))
+    .catch(next);
+};
+
 module.exports = {
   getUsers,
   getUser,
+  createUser,
 };
