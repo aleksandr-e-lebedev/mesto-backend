94c8b9d741b74099e6a2fb22d03eb352bcb1e134
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-02 15:15:39 +0300

    Created the getCards controller

---
+++ controllers/cards.js
@@ -0,0 +1,12 @@
+const Card = require('../models/card');
+
+const getCards = (req, res, next) => {
+  Card.find()
+    .populate(['owner', 'likes'])
+    .then((cards) => res.send(cards))
+    .catch(next);
+};
+
+module.exports = {
+  getCards,
+};
