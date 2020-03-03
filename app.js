96819add3b4deda9465aad4f15ed36ac83e34779
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-03 18:26:24 +0300

    Removed the following application-level middleware: express.static()

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const path = require('path');
 
 const { PORT, DB } = require('./configuration/config');
 const routes = require('./routes');
@@ -16,7 +15,6 @@ mongoose.connect(DB, {
 
 app.use(authorization);
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 app.use(errorHandler);
 
