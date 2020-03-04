cf89c71a77ce579a1ffcea54b7ebda10e729da26
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-05 00:22:11 +0300

    Added the following constant to the constants.js file: NOT_FOUND

--- configuration/constants.js
+++ configuration/constants.js
@@ -1,9 +1,11 @@
+const NOT_FOUND = 'Запрашиваемый ресурс не найден';
 const USER_NOT_FOUND = 'Нет пользователя с таким id';
 const POST_REMOVED = 'Пост удалён';
 const NOT_ENOUGH_RIGHTS = 'Недостаточно прав';
 const SERVER_ERROR = 'Что-то пошло не так';
 
 module.exports = {
+  NOT_FOUND,
   USER_NOT_FOUND,
   POST_REMOVED,
   NOT_ENOUGH_RIGHTS,
