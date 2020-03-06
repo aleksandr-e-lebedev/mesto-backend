b6c4d2336ab2d9b04d7b53b072c60f9bbfb7cfb6
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-06 21:13:35 +0300

    Defined new routes for the cards router
    1) PUT /cards/:cardId/likes
    2) DELETE /cards/:cardId/likes

--- routes/cards.js
+++ routes/cards.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
-const { getCards, createCard, removeCard } = require('../controllers/cards');
+const {
+  getCards, createCard, removeCard, toggleCardLike,
+} = require('../controllers/cards');
 
 router.get('/', getCards);
 router.post('/', createCard);
 router.delete('/:cardId', removeCard);
+router.put('/:cardId/likes', toggleCardLike);
+router.delete('/:cardId/likes', toggleCardLike);
 
 module.exports = router;
