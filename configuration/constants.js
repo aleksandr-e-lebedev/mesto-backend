378050197e36a7d981c14362e3c034573bf5068e
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-10 16:40:45 +0300

    Added the constants for the 'user' and the 'card' schemas to the constants.js file
    1) USER_NAME_REQUIRED,
    2) USER_NAME_MIN_LENGTH,
    3) USER_NAME_MAX_LENGTH,
    4) USER_ABOUT_REQUIRED,
    5) USER_ABOUT_MIN_LENGTH,
    6) USER_ABOUT_MAX_LENGTH,
    7) USER_AVATAR_REQUIRED,
    8) USER_AVATAR_IS_URL
    
    1) CARD_NAME_REQUIRED,
    2) CARD_NAME_MIN_LENGTH,
    3) CARD_NAME_MAX_LENGTH,
    4) CARD_LINK_REQUIRED,
    5) CARD_LINK_IS_URL,
    6) CARD_OWNER_REQUIRED

--- configuration/constants.js
+++ configuration/constants.js
@@ -7,6 +7,22 @@ const SERVER_ERROR = 'Что-то пошло не так';
 const INVALID_REQUEST = 'Запрос сформирован неправильно';
 const INVALID_INPUT_DATA = 'Предоставлены некорректные данные';
 
+const USER_NAME_REQUIRED = 'Пожалуйста, укажите своё имя';
+const USER_NAME_MIN_LENGTH = 'Имя должно содержать не менее 2 символов';
+const USER_NAME_MAX_LENGTH = 'Имя должно содержать не более 30 символов';
+const USER_ABOUT_REQUIRED = 'Пожалуйста, укажите информацию о себе';
+const USER_ABOUT_MIN_LENGTH = 'Информация о себе должна содержать не менее 2 символов';
+const USER_ABOUT_MAX_LENGTH = 'Информация о себе должна содержать не более 30 символов';
+const USER_AVATAR_REQUIRED = 'Пожалуйста, предоставьте своё фото';
+const USER_AVATAR_IS_URL = 'Некорректный формат ссылки на фото для пользователя';
+
+const CARD_NAME_REQUIRED = 'У карточки должно быть название';
+const CARD_NAME_MIN_LENGTH = 'Название карточки должно содержать не менее 2 символов';
+const CARD_NAME_MAX_LENGTH = 'Название карточки должно содержать не более 30 символов';
+const CARD_LINK_REQUIRED = 'У карточки должна быть ссылка на картинку';
+const CARD_LINK_IS_URL = 'Некорректный формат ссылки на фото для карточки';
+const CARD_OWNER_REQUIRED = 'У карточки должен быть владелец';
+
 module.exports = {
   NOT_FOUND,
   USER_NOT_FOUND,
@@ -16,4 +32,20 @@ module.exports = {
   SERVER_ERROR,
   INVALID_REQUEST,
   INVALID_INPUT_DATA,
+
+  USER_NAME_REQUIRED,
+  USER_NAME_MIN_LENGTH,
+  USER_NAME_MAX_LENGTH,
+  USER_ABOUT_REQUIRED,
+  USER_ABOUT_MIN_LENGTH,
+  USER_ABOUT_MAX_LENGTH,
+  USER_AVATAR_REQUIRED,
+  USER_AVATAR_IS_URL,
+
+  CARD_NAME_REQUIRED,
+  CARD_NAME_MIN_LENGTH,
+  CARD_NAME_MAX_LENGTH,
+  CARD_LINK_REQUIRED,
+  CARD_LINK_IS_URL,
+  CARD_OWNER_REQUIRED,
 };
