0102e779e2f2dd73a1752310d56a623ae68c36ee
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-10 17:12:29 +0300

    Set the custom error messages to the 'user' schema

--- models/user.js
+++ models/user.js
@@ -1,29 +1,30 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+
 const { setUpdateOptions } = require('../middlewares');
+const consts = require('../configuration/constants');
 
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    required: [true, consts.USER_NAME_REQUIRED],
+    minlength: [2, consts.USER_NAME_MIN_LENGTH],
+    maxlength: [30, consts.USER_NAME_MAX_LENGTH],
   },
   about: {
     type: String,
-    required: true,
-    minlength: 2,
-    maxlength: 30,
+    required: [true, consts.USER_ABOUT_REQUIRED],
+    minlength: [2, consts.USER_ABOUT_MIN_LENGTH],
+    maxlength: [30, consts.USER_ABOUT_MAX_LENGTH],
   },
   avatar: {
     type: String,
-    required: true,
+    required: [true, consts.USER_AVATAR_REQUIRED],
+    validate: [validator.isURL, consts.USER_AVATAR_IS_URL],
   },
 });
 
 userSchema.pre('findOneAndUpdate', setUpdateOptions);
-
-userSchema.path('avatar').validate(validator.isURL);
 
 userSchema.set('versionKey', false);
 
