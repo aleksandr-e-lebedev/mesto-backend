5b455ecdeae89de291a4123e0ffe64c957326126
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-11 19:26:24 +0300

    Created the pre validator for the operation requests with the user collection

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const authorization = require('./authorization');
 const errorHandler = require('./errorHandler');
 const setUpdateOptions = require('./setUpdateOptions');
+const userPreValidator = require('./userPreValidator');
 
 module.exports = {
   authorization,
   errorHandler,
   setUpdateOptions,
+  userPreValidator,
 };
