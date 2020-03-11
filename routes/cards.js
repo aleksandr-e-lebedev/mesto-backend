34378495282321086bb527ec66b8fb4fc88812f0
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-11 19:52:44 +0300

    Mounted the cardPreValidator to the cards router

--- routes/cards.js
+++ routes/cards.js
@@ -2,9 +2,12 @@ const router = require('express').Router();
 const {
   getCards, createCard, removeCard, toggleCardLike,
 } = require('../controllers/cards');
+const { cardPreValidator } = require('../middlewares');
+
+const { createCardReqCheck } = cardPreValidator;
 
 router.get('/', getCards);
-router.post('/', createCard);
+router.post('/', createCardReqCheck, createCard);
 router.delete('/:cardId', removeCard);
 router.put('/:cardId/likes', toggleCardLike);
 router.delete('/:cardId/likes', toggleCardLike);
