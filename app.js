ea09532bad2459fcdff9e587ababa29370abe31a
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-11 22:28:17 +0300

    Added the requestLogger and the errorLogger to the application level

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const express = require('express');
 
 const routes = require('./routes');
-const { authorization, errorHandler } = require('./middlewares');
+const { authorization, errorHandler, logger } = require('./middlewares');
 
 const app = express();
 
 app.use(authorization);
 app.use(express.json());
+app.use(logger.requestLogger);
 app.use(routes);
+app.use(logger.errorLogger);
 app.use(errorHandler);
 
 module.exports = app;
