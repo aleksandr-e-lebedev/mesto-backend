4039a5bc6043b3fc8dedba9d125421c02d3f927f
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-11 19:36:15 +0300

    Created the pre validator for the operation requests with the card collection

--- middlewares/index.js
+++ middlewares/index.js
@@ -2,10 +2,12 @@ const authorization = require('./authorization');
 const errorHandler = require('./errorHandler');
 const setUpdateOptions = require('./setUpdateOptions');
 const userPreValidator = require('./userPreValidator');
+const cardPreValidator = require('./cardPreValidator');
 
 module.exports = {
   authorization,
   errorHandler,
   setUpdateOptions,
   userPreValidator,
+  cardPreValidator,
 };
