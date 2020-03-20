1384e0274f0245506c4290c09a03416e6c9a39bb
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-20 19:58:20 +0300

    Added the routes to the 'users' router: POST /signup, POST /signin
    Removed the following route: POST /

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,22 @@
 const router = require('express').Router();
+const { createUser, login } = require('../controllers/authController');
 const {
-  getUsers, getUser, createUser, updateUserData, updateUserAvatar,
+  getUsers, getUser, updateUserData, updateUserAvatar,
 } = require('../controllers/users');
 const { userPreValidator } = require('../middlewares');
 
 const {
   createUserReqCheck,
+  loginReqCheck,
   updateUserDataReqCheck,
   updateUserAvatarReqCheck,
 } = userPreValidator;
 
+router.post('/signup', createUserReqCheck, createUser);
+router.post('/signin', loginReqCheck, login);
+
 router.get('/', getUsers);
 router.get('/:userId', getUser);
-router.post('/', createUserReqCheck, createUser);
 router.patch('/me', updateUserDataReqCheck, updateUserData);
 router.patch('/me/avatar', updateUserAvatarReqCheck, updateUserAvatar);
 
