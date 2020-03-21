a8472450802ae7fa7a2e086be5ecb3cf8de59de5
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-21 15:13:22 +0300

    Mounted the cookie-parser to the app level

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 
 const routes = require('./routes');
 const { authorization, logger } = require('./middlewares');
@@ -7,9 +8,13 @@ const errorHandler = require('./controllers/errorController');
 const app = express();
 
 app.use(authorization);
+
 app.use(express.json());
+app.use(cookieParser());
+
 app.use(logger.requestLogger);
 app.use(routes);
+
 app.use(logger.errorLogger);
 app.use(errorHandler);
 
