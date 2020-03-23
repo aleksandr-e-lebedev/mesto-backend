e49538005d65a4009b2dc6fa2c286947b56b2c32
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-23 23:16:24 +0300

    Mounted the rateLimiter to the app level

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 
+const rateLimiter = require('./utils/rateLimiter');
 const routes = require('./routes');
 const { logger } = require('./middlewares');
 const errorHandler = require('./controllers/errorController');
 
 const app = express();
+
+app.use(rateLimiter);
 
 app.use(express.json());
 app.use(cookieParser());
