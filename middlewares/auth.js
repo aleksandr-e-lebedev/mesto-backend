fa083cb29d92e75cc4ecc428b192585ba8821fff
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-28 22:14:02 +0300

    Refactored the auth middleware

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,17 +1,10 @@
 const { promisify } = require('util');
 const jwt = require('jsonwebtoken');
 
-const UnauthorizedError = require('../errors/UnauthorizedError');
-const { AUTH_REQUIRED } = require('../configuration/constants');
 const { JWT_SECRET } = require('../configuration/config');
 
 module.exports = (req, res, next) => {
   const { jwt: token } = req.cookies;
-
-  if (!token) {
-    next(new UnauthorizedError(AUTH_REQUIRED));
-    return;
-  }
 
   promisify(jwt.verify)(token, JWT_SECRET)
     .then((payload) => {
