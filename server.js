14db8ef555349ec3f21e78423ece60b8b3c538fb
Aleksandr Lebedev <lebe.dev@outlook.com>
2020-03-31 18:07:27 +0300

    Overwrote the logic of loading env variables from a .env file

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+if (process.env.NODE_ENV !== 'development') dotenv.config();
 
 const exceptionLogger = require('./utils/exceptionLogger');
 
